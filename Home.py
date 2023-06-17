d328301c5936bc94150284f3b3a31d327bcbd7d5
FadyNos-hy <90998982+FadyNos-hy@users.noreply.github.com>
2023-06-17 03:04:46 +0300

    Add files via upload

--- Home.py
+++ Home.py
@@ -1,33 +1,29 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from  PIL import Image
 from pathlib import Path
 import base64
-
+from utils import *
 
 def main():
+
+    # Congfiger the page attributes
     st.set_page_config(
     page_title='Ksa Vacation',
     page_icon=":Travel:",
     layout="wide")
+
+    # Set the background and the logo
+    set_background('vacation.png')
     add_logo("logo1.png")
-    col1,col2, col3= st.columns( [0.2,0.5, 0.1])
+
+    # Write down the title in the middle of the page
+    _,col2,_= st.columns( [0.2,0.5, 0.1])
     with col2:
         st.markdown('#\n#\n#') 
         st.markdown(f'<h1 style="color:#141517;font-family: "Cooper Black";font-size:70px;">{"Want to plan your Ksa vacation?"}</h1>', unsafe_allow_html=True)
 
-    set_background('vacation.png')
-   
-    
-    
-    
-
+# function to render the logo image at the side bar
 def add_logo(logo_url: str, height: int = 400):
-
-
     logo = f"url(data:image/png;base64,{base64.b64encode(Path(logo_url).read_bytes()).decode()})"
-
     st.markdown(
         f"""
         <style>
@@ -43,26 +39,6 @@ def add_logo(logo_url: str, height: int = 400):
         """,
         unsafe_allow_html=True,
     )
-
-
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
-
 
 if __name__ == "__main__":
     main()
---
+++ utils.py
@@ -0,0 +1,21 @@
+import base64
+import streamlit as st
+
+OPENAI_API_KEY = "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"
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
