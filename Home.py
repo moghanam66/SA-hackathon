f398e8a242587a0e269305582f828d52499ce46c
FadyNos-hy <90998982+FadyNos-hy@users.noreply.github.com>
2023-06-17 17:00:04 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -24,12 +24,12 @@ def main():
     # Write down the title in the middle of the page
     _,col2,_= st.columns( [0.3,0.5, 0.1])
     with col2:
-        st.title('Travelogue')
-        st.header('Launch your joy')
+        st.markdown(f"""<h1 style="color:#41ba61;font-family: "Cooper Black";">Travelogue</h1>""", unsafe_allow_html=True))
+        st.markdown(f"""<h4 >Launch your joy</h4>""", unsafe_allow_html=True))
 
     
     st.divider()
-    st.markdown(f"""<h2 style="color:#AFE1AF;font-family: "Cooper Black";font-size:50px;">Are you planning a trip to Saudi Arabia but don't know where to start? Have you ever seen a photo of a place you like and wondered where you can find something similar in Saudi Arabia?\n\n</h1>""", unsafe_allow_html=True)
+    st.markdown(f"""<h2 style="color:#41ba61;font-family: "Cooper Black";">Are you planning a trip to Saudi Arabia but don't know where to start? Have you ever seen a photo of a place you like and wondered where you can find something similar in Saudi Arabia?\n\n</h2>""", unsafe_allow_html=True)
     st.empty()
     st.markdown("<h4>If so, then you've come to the right place! Our website is your one-stop shop for all things Saudi Arabia tourism. We have everything you need to plan your perfect trip and information on the best places to visit (supports English and Arabic languages).\n\n</h4>", unsafe_allow_html=True)
 
