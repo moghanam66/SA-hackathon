cab42e5aea64e4ba6b24c9a4ac28411535d95b43
mohamed.elghannam <mohamed.elghannam@fixedmea.com>
2023-06-17 14:02:04 +0300

    resize logo

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ def main():
 
     # Set the background and the logo
     set_background('vacation.jpeg')
-    add_logo("logo1.png")
+    add_logo("logo1.jpg")
 
     # Write down the title in the middle of the page
     _,col2,_= st.columns( [0.2,0.5, 0.1])
@@ -25,10 +25,10 @@ def main():
     }
     </style>
     """, unsafe_allow_html=True)
-    st.markdown('<p class="big-font">This platform offers :\n</p>', unsafe_allow_html=True)
-    st.markdown('<p class="big-font">1- Plan your vacation through Saudi Arabia, just say what you want to do (supports English and Arabic languages).\n\n</p>', unsafe_allow_html=True)
-    st.markdown('<p class="big-font">2- Search by an image of what activity or place you want to see in Saudi Arabia.\n\n</p>', unsafe_allow_html=True)
-    st.markdown('<p class="big-font">3- Ask about any concern you want to investigate before going to Saudi Arabia (supports English and Arabic languages).\n</p>', unsafe_allow_html=True)
+    # st.markdown('<p class="big-font">This platform offers :\n</p>', unsafe_allow_html=True)
+    st.markdown('<p class="big-font">Plan your vacation through Saudi Arabia, just say what you want to do (supports English and Arabic languages).\n\n</p>', unsafe_allow_html=True)
+    st.markdown('<p class="big-font">Search by an image of what activity or place you want to see in Saudi Arabia.\n\n</p>', unsafe_allow_html=True)
+    st.markdown('<p class="big-font">Ask about any concern you want to investigate before going to Saudi Arabia (supports English and Arabic languages).\n</p>', unsafe_allow_html=True)
         
         
         
