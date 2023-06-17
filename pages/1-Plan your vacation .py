d8b2dc4359100caad364e47ada5e34225535861b
FadyNos-hy <90998982+FadyNos-hy@users.noreply.github.com>
2023-06-17 03:05:12 +0300

    Add files via upload

--- pages/1-Plan your vacation .py
+++ pages/1-Plan your vacation .py
@@ -5,90 +5,95 @@ from deep_translator import GoogleTranslator
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain,SequentialChain
 from langchain.prompts import ChatPromptTemplate
-import base64
+from utils import *
 
+def main():
+
+    # Congfiger the page attributes
+    st.set_page_config(
+        page_title='Plan your vacation',
+        page_icon=":star:",layout="wide")
+
+    # Set the background
+    set_background('wallpaper.jpeg')
+
+    # Set the title
+    st.title('Tell us what you want to do and let AI plan your stay for you.')
+
+    # Read the scrapped data of the attraction sites
+    df=pd.read_csv('attractionsEng.csv')
+
+    # Get all the names of the attraction sites
+    placesDf=list(df['attractionSite'])
+
+    # Ask the user what he want to do 
+    userInput = st.text_input(" What do you want to do in Saudi ?",'I want to visit fun sites')
+
+    if userInput !='':
+        #Get the plane and the places to visit that was introduced by chat GBT
+        answer=openAiPlaner(userInput,placesDf,OPENAI_API_KEY)
+        plane=answer['plane']
+        places=answer['places'].split('\n')
+
+        # Split the plans to days and translate it into Arabic if the question is in Arabic
+        days=plane.split('\n\n')
+        for day in days:
+            if re.fullmatch('^[\u0621-\u064A0-9 ]+$',userInput):
+                dayAr=GoogleTranslator(source='en',target='ar').translate(day).replace('-','\n-')
+                st.markdown("<div style='direction: RTL;'> {} </div>".format(dayAr), unsafe_allow_html=True)
+            else:
+                st.markdown(day)
+
+            # Get all the places mentioned in each day to extract images of the same places
+            placesToDisplay=[place[3:].strip() for place in places if place[3:].strip() in day]
+            images=[]
+            captions=[]
+            for placeToDisplay in placesToDisplay:
+                image=df[df['attractionSite']==placeToDisplay]['image'].values
+
+                if len(image)>0:
+                    # Translate the caption into Arabic if the question is in Arabic
+                    if re.fullmatch('^[\u0621-\u064A0-9 ]+$',userInput):
+                        placeToDisplay=GoogleTranslator(source='en',target='ar').translate(placeToDisplay)
+                    images.append(image[0])
+                    captions.append(placeToDisplay)
+
+            # Display the images of the places mentioned in this day
+            st.image(images,caption=captions,width=300)
+
+# Cache the answer to optimize the preformance and reduce any waisted cost. 
 @st.cache_data
 def openAiPlaner(question,placesDf,API_KEY):
+
+    #Intialize the LLM
     llm = ChatOpenAI(openai_api_key=API_KEY,temperature=0)
 
-    # prompt template 1: translate to english
+    # prompt template 1: Get all the suitable palces
     first_prompt = ChatPromptTemplate.from_template(
         "This is a question from a tourist visiting Saudi Arabia:"
         "\n\n{Question}"
         f"\n\n Suggest 10 places to visit from this list{placesDf}")
 
-    # chain 1: input= Review and output= English_Review
+    # chain 1: input= Question and output= places
     chain_one = LLMChain(llm=llm, prompt=first_prompt,
                         output_key="places")
-    # prompt template 1: translate to english
+    
+    # prompt template 2: Create a plan to visit those places
     second_prompt = ChatPromptTemplate.from_template(
         "Create a plan to visit those places:"
         "\n\n{places}")
-
+    
+    # chain 2: input= places and output= plan
     chain_two = LLMChain(llm=llm, prompt=second_prompt,
                         output_key="plane")
+    
+    #Include all chains and create the sequential chain
     overall_chain = SequentialChain(chains=[chain_one,chain_two],
     input_variables=["Question"],
     output_variables=["places",'plane'],
     verbose=True)
+
     return overall_chain(question)
 
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    
-
-st.set_page_config(
-    page_title='Plan your vacation',
-    page_icon=":star:",layout="wide")
-
-# set_background('_2f6e1e1c-5229-4fce-8241-9beea9c29c5d.jpeg')
-
-OPENAI_API_KEY = st.secrets["openAI_API"]
-st.title('Tell us what you want to do and let AI plan your stay for you.')
-
-dfEng=pd.read_csv('attractionsEng.csv')
-placesDf=list(dfEng['attractionSite'])
-
-
-user_input = st.text_input(" What do you want to do in Saudi ?",'I want to visit fun sites')
-if user_input !='':
-    
-    answer=openAiPlaner(user_input,placesDf,OPENAI_API_KEY)
-    plane=answer['plane']
-    places=answer['places'].split('\n')
-    days=plane.split('\n\n')
-    for day in days:
-        if re.fullmatch('^[\u0621-\u064A0-9 ]+$',user_input):
-            dayAr=GoogleTranslator(source='en',target='ar').translate(day).replace('-','\n-')
-            st.markdown("<div style='direction: RTL;'> {} </div>".format(dayAr), unsafe_allow_html=True)
-        else:
-            st.markdown(day)
-        placesToDisplay=[place[3:].strip() for place in places if place[3:].strip() in day]
-        nuOfImages=plane.count('-')
-        images=[]
-        captions=[]
-        for placeToDisplay in placesToDisplay:
-            image=dfEng[dfEng['attractionSite']==placeToDisplay]['image'].values
-            if len(image)>0:
-                if re.fullmatch('^[\u0621-\u064A0-9 ]+$',user_input):
-                    placeToDisplay=GoogleTranslator(source='en',target='ar').translate(placeToDisplay)
-                images.append(image[0])
-                captions.append(placeToDisplay)
-        st.image(images,caption=captions,width=300)
-
-
-    
+if __name__ == "__main__":
+    main()    
--- pages/2-Search by image.py
+++ pages/2-Search by image.py
@@ -1,25 +1,93 @@
+import os
 import streamlit as st
 import tensorflow as tf
 import tensorflow_hub as hub
+import pandas as pd
 import numpy as np
 from annoy import AnnoyIndex
-import os
 from PIL import Image
-import pandas as pd
 import openai
+from utils import *
 
-st.set_page_config(
-    page_title='Search by image',
-    page_icon=":star:",layout="wide")
+def main():
 
-df = pd.read_csv('attractionsEng.csv')
-openai.api_key =  st.secrets["openAI_API"]
+    # Congfiger the page attributes
+    st.set_page_config(
+        page_title='Search by image',
+        page_icon=":star:",layout="wide")
+    
+    # Set the background
+    set_background('wallpaper.jpeg')
 
+    # Set the title
+    st.title("Image Similarity Search")
 
+    # Set a css style for markdown to increase its font size
+    st.markdown("""
+    <style>
+    .big-font {
+        font-size:25px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    st.markdown('''<p class="big-font">Have you visited a place and asked yourself if there is someplace like this in Saudi Arabia? Or are you searching for a specific vibe, and where can you find it in Saudi Arabia?
+    Upload a photo of your desired location, and we'll help you find the most similar place to it and where to find it.</p>''', unsafe_allow_html=True)
 
-# Load the MobileNetV2 model from TensorFlow Hub
-model_url = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4"
-model = tf.keras.Sequential([hub.KerasLayer(model_url, input_shape=(224, 224, 3))])
+    # Read the scrapped data of the attraction sites
+    df = pd.read_csv('attractionsEng.csv')
+
+    # Get the image from the user
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg",'png','jpeg'])
+
+    # Load the MobileNetV2 model from TensorFlow Hub
+    model_url = "https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4"
+    model = tf.keras.Sequential([hub.KerasLayer(model_url, input_shape=(224, 224, 3))])
+
+    #Create a vector reparesnting the images in the folder cvs_images to compare it with the input image
+    annoy_index,image_files,image_folder=loadImages(model)
+
+    if uploaded_file is not None:
+
+        # load the input image and display it
+        user_img = Image.open(uploaded_file)
+        _,col2,_=st.columns([0.2,0.4,0.2])
+        with col2:
+            st.image(user_img, caption="Uploaded Image", use_column_width=True)
+
+        # preproces the image and predict the vector representing theimage
+        user_img = load_and_preprocess_image(user_img)
+        user_features = model.predict(np.expand_dims(user_img, axis=0))
+
+        # Find the most similar image in the dataset and load it
+        n_nearest_neighbors = 1
+        nearest_indices = annoy_index.get_nns_by_vector(user_features.squeeze(), n_nearest_neighbors)
+        nearest_image_file = image_files[nearest_indices[0]]
+        similar_image = Image.open(os.path.join(image_folder, nearest_image_file))
+
+        # Represent the similar image and the location of the place presented by the image
+        col1,col2=st.columns([0.5,0.5])
+        with col1:
+            st.image(similar_image, caption="Similar Image", use_column_width=True)
+        with col2:
+            # get the row of the dataset representing the attraction site
+            index_without_extension = int(nearest_image_file.split('.')[0])
+            row = df.loc[index_without_extension]
+
+            # Define the coordinates (latitude, longitude) of the place
+            coordinates = {'latitude': [row['Latitude']], 'longitude': [row['Longitude']]}
+
+            # Create a DataFrame with the coordinates
+            df_coordinates = pd.DataFrame(coordinates)
+
+            # Display the map centered on the coordinates
+            st.map(df_coordinates)
+        
+        #Display all the info about the placr from the scrapped data
+        sentence = f"The place : {row['attractionSite']}, it's located in  {row['city']}, {row['description']}."
+        st.title(sentence)
+
+        # Display MORE infroemation through chat GPT
+        st.title(openAiDescription(row['attractionSite']))
 
 # Function to load and preprocess images
 def load_and_preprocess_image(image):
@@ -28,8 +96,9 @@ def load_and_preprocess_image(image):
     img = np.array(img) / 255.0  # Normalize image pixels to [0, 1]
     return img
 
+# Casche the features of each image to increase the preformance
 @st.cache_resource
-def loadImages():
+def loadImages(_model):
     # Load dataset images and extract features
     image_folder = 'csv_images'
     image_files = os.listdir(image_folder)
@@ -39,7 +108,7 @@ def loadImages():
         img = Image.open(os.path.join(image_folder, image_file))
         img = load_and_preprocess_image(img)
         img = np.expand_dims(img, axis=0)  # Add batch dimension
-        features = model.predict(img)
+        features = _model.predict(img) # predict the features
         image_features.append(features.squeeze())
 
     # Build Annoy index
@@ -47,16 +116,18 @@ def loadImages():
     for i, feature in enumerate(image_features):
         annoy_index.add_item(i, feature)
     annoy_index.build(10)
-    return annoy_index,image_files,image_folder
-annoy_index,image_files,image_folder=loadImages()
 
-###################################################################
+    return annoy_index,image_files,image_folder
+
+# Cache the answer to optimize the preformance and reduce any waisted cost. 
+@st.cache_data
 def openAiDescription(place):
+    openai.api_key = OPENAI_API_KEY 
     # Set the model and prompt
+    model_engine = "text-davinci-003"
     prompt =  f"Give a describtion about {place} in less than 70 words about in Saudi Arabia"
 
     # Set the maximum number of tokens to generate in the response
-    model_engine = "text-davinci-003"
     max_tokens = 1024
     # Generate a response
     completion = openai.Completion.create(
@@ -69,53 +140,7 @@ def openAiDescription(place):
             presence_penalty=0
         )
     return completion.choices[0].text
-st.title("Image Similarity Search")
-st.markdown("""
-<style>
-.big-font {
-    font-size:25px !important;
-}
-</style>
-""", unsafe_allow_html=True)
-st.markdown('''<p class="big-font">Have you visited a place and asked yourself if there is someplace like this in Saudi Arabia? Or are you searching for a specific vibe, and where can you find it in Saudi Arabia?
-Upload a photo of your desired location, and we'll help you find the most similar place to it and where to find it.</p>''', unsafe_allow_html=True)
-
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg",'png','jpeg'])
 
 
-if uploaded_file is not None:
-    user_img = Image.open(uploaded_file)
-    st.image(user_img, caption="Uploaded Image", use_column_width=True)
-
-    user_img = load_and_preprocess_image(user_img)
-    user_features = model.predict(np.expand_dims(user_img, axis=0))
-
-    # Find the most similar image in the dataset
-    n_nearest_neighbors = 1
-    nearest_indices = annoy_index.get_nns_by_vector(user_features.squeeze(), n_nearest_neighbors)
-    nearest_image_file = image_files[nearest_indices[0]]
-
-    similar_image = Image.open(os.path.join(image_folder, nearest_image_file))
-    col1,col2=st.columns([0.5,0.5])
-    with col1:
-        st.image(similar_image, caption="Similar Image", use_column_width=True)
-    with col2:
-        index_without_extension = int(nearest_image_file.split('.')[0])
-
-        row = df.loc[index_without_extension]
-        # Create a sentence from column values (touristic places)
-        # Define the coordinates (latitude, longitude)
-        coordinates = {'latitude': [row['Latitude']], 'longitude': [row['Longitude']]}
-
-        # Create a DataFrame with the coordinates
-        df_coordinates = pd.DataFrame(coordinates)
-
-        # Display the map centered on the coordinates
-        st.map(df_coordinates)
-    sentence = f"The place : {row['attractionSite']}, it's located in  {row['city']}, {row['description']}."
-    st.title(sentence)
-
-    
-    st.title(openAiDescription(row['attractionSite']))
-
-
+if __name__ == "__main__":
+    main()  
