7e0e9e87bb72e8b8a0cf2eda2bd5c8c0e44f1393
FadyNos-hy <90998982+FadyNos-hy@users.noreply.github.com>
2023-06-17 17:00:54 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -25,7 +25,7 @@ def main():
     _,col2,_= st.columns( [0.3,0.5, 0.1])
     with col2:
         st.markdown(f"""<h1 style="color:#41ba61;font-family: "Cooper Black";">Travelogue</h1>""", unsafe_allow_html=True)
-        st.markdown(f"""<h4 >Launch your joy</h4>""", unsafe_allow_html=True))
+        st.markdown(f"""<h4 >Launch your joy</h4>""", unsafe_allow_html=True)
 
     
     st.divider()
